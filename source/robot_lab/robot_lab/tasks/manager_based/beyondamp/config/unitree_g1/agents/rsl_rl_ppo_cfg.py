5c1a2f5b27fff7575a9382af8388c77ac4f9b3eb
JiaJunH810 <1287971889@qq.com>
2026-06-17 16:05:20 +0800

    add unitree_g1 beyondmap

---
+++ source/robot_lab/robot_lab/tasks/manager_based/beyondamp/config/unitree_g1/agents/rsl_rl_ppo_cfg.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2024-2025 Ziqi Fan
+# SPDX-License-Identifier: Apache-2.0
+
+from isaaclab.utils import configclass
+from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, RslRlPpoAlgorithmCfg
+
+from robot_lab.tasks.manager_based.beyondamp.rsl_rl_amp.amp_on_policy_runner import AMPOnPolicyRunner
+from robot_lab.tasks.manager_based.beyondamp.rsl_rl_amp.amp_vecenv_wrapper import AMPRslRlVecEnvWrapper
+
+@configclass
+class G1BeyondAMPFlatPPORunnerCfg(RslRlOnPolicyRunnerCfg):
+    num_steps_per_env = 24
+    max_iterations = 30000
+    save_interval = 500
+    experiment_name = "g1_beyondamp"
+    runner_type = AMPOnPolicyRunner
+    wrapper_type = AMPRslRlVecEnvWrapper
+
+    amp_discr_hidden_dims = [1024, 512, 256]
+    amp_reward_coef = 0.1
+    amp_task_reward_lerp = 0.75
+    amp_min_normalized_std = 0.05
+    amp_replay_buffer_size = 100000
+
+    policy = RslRlPpoActorCriticCfg(
+        init_noise_std=1.0,
+        actor_obs_normalization=True,
+        critic_obs_normalization=True,
+        actor_hidden_dims=[512, 256, 128],
+        critic_hidden_dims=[512, 256, 128],
+        activation="elu",
+    )
+    algorithm = RslRlPpoAlgorithmCfg(
+        class_name = "rsl_rl_amp.amp_ppo:AMPPPO",
+        value_loss_coef=1.0,
+        use_clipped_value_loss=True,
+        clip_param=0.2,
+        entropy_coef=0.005,
+        num_learning_epochs=5,
+        num_mini_batches=4,
+        learning_rate=1.0e-3,
+        schedule="adaptive",
+        gamma=0.99,
+        lam=0.95,
+        desired_kl=0.01,
+        max_grad_norm=1.0,
+    )
---
+++ source/robot_lab/robot_lab/tasks/manager_based/beyondamp/config/unitree_g1/flat_env_cfg.py
@@ -0,0 +1,65 @@
+# Copyright (c) 2024-2025 Ziqi Fan
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+
+from isaaclab.utils import configclass
+
+from robot_lab.assets.unitree import UNITREE_G1_29DOF_ACTION_SCALE, UNITREE_G1_29DOF_CFG
+from robot_lab.tasks.manager_based.beyondamp.tracking_env_cfg import CyborgEnvCfg
+import robot_lab.tasks.manager_based.beyondamp.obs_groups as amp_groups
+
+
+@configclass
+class G1BeyondAMPFlatEnvCfg(CyborgEnvCfg):
+    """Unitree G1 beyondAMP 环境配置。
+
+    amp 观测组已通过 tracking_env_cfg.py 的 ObservationsCfg 继承，
+    body 数据过滤由 MotionCommand.body_indexes 自动完成。
+    """
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        # Robot
+        self.scene.robot = UNITREE_G1_29DOF_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+        self.actions.joint_pos.scale = UNITREE_G1_29DOF_ACTION_SCALE
+
+        # Motion (anchor is pelvis to match pre-processed motion data)
+        self.commands.motion.motion_file = f"{os.path.dirname(__file__)}/motion/"
+        self.commands.motion.anchor_body_name = "pelvis"
+        self.commands.motion.body_names = [
+            "pelvis",
+            "left_hip_roll_link", "left_knee_link", "left_ankle_roll_link",
+            "right_hip_roll_link", "right_knee_link", "right_ankle_roll_link",
+            "left_shoulder_roll_link", "left_elbow_link", "left_wrist_yaw_link",
+            "right_shoulder_roll_link", "right_elbow_link", "right_wrist_yaw_link",
+        ]
+        self.commands.motion.amp_obs_terms = amp_groups.AMPObsHardTrackTerms
+        self.episode_length_s = 30.0
+
+        # Override Cyborg-specific event body names for G1
+        self.events.randomize_com_positions.params["asset_cfg"].body_names = ("pelvis",)
+
+        # Override feet_slide body names
+        self.rewards.feet_slide.params["asset_cfg"].body_names = [
+            "left_ankle_roll_link", "right_ankle_roll_link",
+        ]
+
+        # Override self_collisions body names (use torso_link for upper body collisions)
+        self.rewards.self_collisions.params["sensor_cfg"].body_names = [
+            "pelvis",
+            "left_shoulder_roll_link", "left_elbow_link", "left_wrist_yaw_link",
+            "right_shoulder_roll_link", "right_elbow_link", "right_wrist_yaw_link",
+        ]
+
+        # G1 termination: lower minimum height (pelvis at 0.76m crouch)
+        self.terminations.bad_base_height.params["minimum_height"] = 0.55
+
+        # G1 track_anchor velocity/anchor config (pelvis as anchor)
+        self.rewards.track_anchor_linear_velocity.params["anchor_cfg"].body_names = ["pelvis"]
+        self.rewards.track_anchor_angular_velocity.params["anchor_cfg"].body_names = ["pelvis"]
+        self.rewards.body_ang_vel_xy_l2.params["body_cfg"].body_names = ["pelvis"]
+
+        # Disable track_head_height (Cyborg-specific)
+        self.rewards.track_head_height.weight = 0.0
