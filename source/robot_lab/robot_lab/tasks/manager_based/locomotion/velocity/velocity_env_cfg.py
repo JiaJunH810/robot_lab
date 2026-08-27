62d2be15847624d1dbc666bdd10bad699220eb3c
JiaJunH810 <1287971889@qq.com>
2026-08-27 16:44:14 +0800

    candidate-14: widen reset yaw and tighten flat tolerance

--- source/robot_lab/robot_lab/tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
+++ source/robot_lab/robot_lab/tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
@@ -339,7 +339,7 @@ class EventCfg:
         mode="startup",
         params={
             "asset_cfg": SceneEntityCfg("robot", body_names=".*"),
-            "com_range": {"x": (-0.055, 0.065), "y": (-0.06, 0.06), "z": (-0.06, 0.06)},
+            "com_range": {"x": (-0.06, 0.06), "y": (-0.06, 0.06), "z": (-0.06, 0.06)},
         },
     )
 
@@ -404,7 +404,7 @@ class EventCfg:
                 "z": (-0.2, 0.2),
                 "roll": (-0.2, 0.2),
                 "pitch": (-0.2, 0.2),
-                "yaw": (-0.6, 0.6),
+                "yaw": (-0.7, 0.7),
             },
         },
     )
