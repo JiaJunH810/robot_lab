fcfd42b2c2edeabd8e5d84f691d3c40ae1e492cb
JiaJunH810 <1287971889@qq.com>
2026-08-31 14:52:47 +0800

    add iteration

--- source/robot_lab/robot_lab/tasks/manager_based/locomotion/velocity/config/humanoid/cyborg/agents/rsl_rl_ppo_cfg.py
+++ source/robot_lab/robot_lab/tasks/manager_based/locomotion/velocity/config/humanoid/cyborg/agents/rsl_rl_ppo_cfg.py
@@ -54,5 +54,5 @@ class CyborgHPFlatPPORunnerCfg(CyborgHPRoughPPORunnerCfg):
     def __post_init__(self):
         super().__post_init__()
 
-        self.max_iterations = 5010
+        self.max_iterations = 6010
         self.experiment_name = "cyborg_hp_flat"
